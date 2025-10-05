75376316ad4489384d007de84722e6351933be95
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-05 22:16:18 +0530

    Minor changes

--- src/mineral/errors.py
+++ src/mineral/errors.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from .window import Window
+from .screen.window import Window
 
 if TYPE_CHECKING:
     from typing import Any, Optional
