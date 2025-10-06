192c8ccb78ff6938eef1ba642da3ccf1c076b5c2
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-06 22:11:11 +0530

    Formatting

--- src/mineral/utils.py
+++ src/mineral/utils.py
@@ -1,8 +1,9 @@
 class FileLogger:
     def __init__(self, path: str) -> None:
         self.path = path
-        with open(path, "w"): ...
-    
+        with open(path, "w"):
+            ...
+
     def file_log(self, text: str) -> None:
         with open(self.path, "a") as file:
-            file.writelines([text+"\n"])
+            file.writelines([text + "\n"])
