eea2fcdef1c54c645861dd6b505a06606a938e43
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-07 11:59:06 +0530

    Converted text gadget to class

--- src/mineral/gadgets.py
+++ src/mineral/gadgets.py
@@ -1,46 +1,63 @@
 from __future__ import annotations
 
 import curses
-import time
 from typing import TYPE_CHECKING, overload
+
+from .types import MISSING
 
 if TYPE_CHECKING:
     from typing import Optional, Tuple
 
 
-class Gadgets:
-    def __init__(self, text_delay: float = 0.1) -> None:
-        self.text_delay: float = text_delay
-
+class Text:
     @overload
-    def text(
+    def __init__(
         self,
         win: curses.window,
         text: str,
+        text_delay: int = 200,
+        auto_start: bool = True,
         *,
         rel_pos: Tuple[float, float] = ...,
         pos: None = ...,
     ) -> None: ...
 
     @overload
-    def text(
+    def __init__(
         self,
         win: curses.window,
         text: str,
+        text_delay: int = 200,
+        auto_start: bool = True,
         *,
         rel_pos: None = ...,
         pos: Tuple[int, int] = ...,
     ) -> None: ...
 
-    def text(
+    def __init__(
         self,
         win: curses.window,
         text: str,
+        text_delay: int = 200,
+        auto_start: bool = True,
         *,
         rel_pos: Optional[Tuple[float, float]] = None,
         pos: Optional[Tuple[int, int]] = None,
     ) -> None:
-        for char in text:
-            if rel_pos:
-                height, width = win.getmaxyx()
-                pos = (round(width * rel_pos[0]), round(height * rel_pos[1]))
+        self.win: curses.window = win
+        self.text: str = text
+        self.text_delay: int = text_delay
+        self.rel_pos: Optional[Tuple[float, float]] = rel_pos
+        self.pos: Tuple[int, int] = pos or MISSING
+        self.running: bool = False
+
+        if auto_start:
+            self.start()
+    
+    def start(self) -> None:
+        self.running = True
+
+        for char in self.text:
+            if self.rel_pos:
+                height, width = self.win.getmaxyx()
+                self.pos = round(width * self.rel_pos[0]), round(height * self.rel_pos[1])
