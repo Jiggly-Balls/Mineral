ab8712067a9bd496a6c1327b4790f4ec75b56d95
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-07 12:35:20 +0530

    Added auto relative adjuster for Text

--- src/mineral/gadgets.py
+++ src/mineral/gadgets.py
@@ -17,6 +17,7 @@ class Text:
         text: str,
         text_delay: int = 200,
         auto_start: bool = True,
+        rel_auto_adjust: bool = True,
         *,
         rel_pos: Tuple[float, float] = ...,
         pos: None = ...,
@@ -29,6 +30,7 @@ class Text:
         text: str,
         text_delay: int = 200,
         auto_start: bool = True,
+        rel_auto_adjust: bool = True,
         *,
         rel_pos: None = ...,
         pos: Tuple[int, int] = ...,
@@ -40,6 +42,7 @@ class Text:
         text: str,
         text_delay: int = 200,
         auto_start: bool = True,
+        rel_auto_adjust: bool = True,
         *,
         rel_pos: Optional[Tuple[float, float]] = None,
         pos: Optional[Tuple[int, int]] = None,
@@ -47,17 +50,33 @@ class Text:
         self.win: curses.window = win
         self.text: str = text
         self.text_delay: int = text_delay
+        self.rel_auto_adjust: bool = rel_auto_adjust
         self.rel_pos: Optional[Tuple[float, float]] = rel_pos
         self.pos: Tuple[int, int] = pos or MISSING
+        
         self.running: bool = False
+        self.finished: bool = False
 
         if auto_start:
             self.start()
     
+    def _calculate_rel_pos(self) -> None:
+        if self.rel_pos:
+            height, width = self.win.getmaxyx()
+            if self.rel_auto_adjust:
+                width -= len(self.text)
+            self.pos = (round(width * self.rel_pos[0]), round(height * self.rel_pos[1]))
+    
     def start(self) -> None:
         self.running = True
 
-        for char in self.text:
-            if self.rel_pos:
-                height, width = self.win.getmaxyx()
-                self.pos = round(width * self.rel_pos[0]), round(height * self.rel_pos[1])
+        if self.text_delay > 0:
+            for char in self.text:
+                self._calculate_rel_pos()
+                column, row = self.pos
+
+        else:
+            self._calculate_rel_pos()
+            column, row = self.pos
+
+            self.win.addstr(row, column, self.text)
